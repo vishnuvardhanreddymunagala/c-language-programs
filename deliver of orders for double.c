eb995f530a84e21ca86d8f5b104302af8bf0ca3c
vishnuvardhanreddymunagala <95745089+vishnuvardhanreddymunagala@users.noreply.github.com>
2021-12-13 14:51:34 +0530

    Add files via upload

---
+++ deliver of orders for double.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+ int b,f,p,s,r;
+ printf("enter the quantity of burger,sandwich,pizza,frienchfries");
+ scanf("%d %d %d %d",&b,&s,&p,&f);
+ r=((200*b)+(150*s)+(100*p)+(50*f));
+ printf("total bill is %d",r);
+ 
+}
+ 
+
+
+
+
+
+
+
+
